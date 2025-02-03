b0a8b37f44b9cc9d9cd759f79407d97129582d0f
farrel irawan <irawanssfarrel@gmail.com>
2025-02-03 22:50:56 +0700

    membuat fungsi penanganan pencarian user pada komponen try feature dan memodifikasi styling

--- src/js/utils/API.js
+++ src/js/utils/API.js
@@ -2,7 +2,7 @@ async function getDataFromAPI(apiUrl) {
     try {
         const response = await fetch(apiUrl);
         const data = await response.json();
-        console.log(data);
+        return data;
     } catch (err) {
         console.error(err);
     }
--- src/js/welcome.script.js
+++ src/js/welcome.script.js
@@ -1,5 +1,8 @@
 import { getDataFromAPI } from "./utils/API.js";
 const navibar = document.querySelector('.navibar');
+const resultContainer = document.querySelector('.try-feature .feature-result');
+const featureSearchInput = document.querySelector('#feature-search');
+const exploreBtn = document.querySelector('.try-feature div button');
 
 function fixedNavOnScroll() {
     if (window.scrollY >= 80) {
@@ -9,8 +12,35 @@ function fixedNavOnScroll() {
     }
 }
 
-getDataFromAPI("https://api.unsplash.com/photos/?client_id=xHA1LFvj7tkMQdgmzgfJH7iCn2BXd5BwJYX4TxLaIv4")
+function onTryFeatureHandler() {
+    resultContainer.innerHTML = "";
 
-window.onload = () => {
-    window.onscroll = fixedNavOnScroll;
+    if (featureSearchInput.value === "") {
+        resultContainer.innerHTML = `
+            <p style="display: inline-block; margin: auto 0; height: fit-content; width: 100%; text-align: center;">Please enter a keyword</p>
+        `;
+
+        return;
+    }
+    
+    try {
+        const data = getDataFromAPI(`https://api.unsplash.com/photos?random=10&client_id=xHA1LFvj7tkMQdgmzgfJH7iCn2BXd5BwJYX4TxLaIv4`);
+        data.then((response) => {
+            response.forEach(data => {
+                console.log(data);
+                resultContainer.insertAdjacentHTML('beforeend', `
+                    <article class="image-card">
+                        <img src="${data.urls.small}" alt="${data.alt_description}">
+                    </article>
+                `);
+            })
+        })
+    } catch (err) {
+        console.error('message error', err);
+        
+    }
 }
+
+
+exploreBtn.onclick = onTryFeatureHandler;
+window.onscroll = fixedNavOnScroll;
