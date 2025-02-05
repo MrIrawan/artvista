d5b13ea616e86ddddae89542be311fe6fc44320c
farrel irawan <irawanssfarrel@gmail.com>
2025-02-05 13:03:54 +0700

    menambahkan scroll animation pada area about dan fitur

--- src/js/welcome.animation.js
+++ src/js/welcome.animation.js
@@ -13,3 +13,24 @@ timeLine.from('.home-item h1', {y: 20, opacity: 0, duration: 1})
         .from('.home-item:nth-child(2)', {y: 20, opacity: 0, duration: 1}, '+=0.6')
 
 
+gsap.from('.about-item', {
+    scrollTrigger: '.about-item',
+    y: 20,
+    opacity: 0,
+    duration: 1
+})
+
+gsap.from('.about-flex-item', {
+    scrollTrigger: '.about-flex-item',
+    x: -20,
+    opacity: 0,
+    duration: 1,
+    stagger: 0.7
+})
+
+gsap.from('.try-feature', {
+    scrollTrigger: '.try-feature',
+    y: 20,
+    opacity: 0,
+    duration: 1
+})
