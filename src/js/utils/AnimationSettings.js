69c5bcd34c60406e48233df43cb05f65f1e03e9d
farrel irawan <irawanssfarrel@gmail.com>
2025-02-05 20:05:28 +0700

    memberikan animasi scroll kepada seluruh area komponen mulai dari hero section hingga ke collections

--- src/js/utils/AnimationSettings.js
+++ src/js/utils/AnimationSettings.js
@@ -1,5 +1,9 @@
 import { gsap } from 'gsap';
 
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(ScrollTrigger);
+
 const fadeToDirection = (element, delay = 0, direction = 'y', value = 50) => {
     gsap.from(element, {
         opacity: 0,
@@ -10,4 +14,20 @@ const fadeToDirection = (element, delay = 0, direction = 'y', value = 50) => {
     });
 };
 
-export { fadeToDirection };
+const triggerAnimation = (element, direction = 'y', value = 50, stack = 0, duration = 1) => {
+    gsap.from(element, {
+        opacity: 0,
+        [direction]: value,
+        duration: duration,
+        ease: "power2.out",
+        stagger: stack,
+        scrollTrigger: {
+            trigger: element,
+            start: "top 80%",
+            toggleActions: "play none none reverse",
+    }
+});
+
+}
+
+export { fadeToDirection, triggerAnimation };
--- src/js/welcome.animation.js
+++ src/js/welcome.animation.js
@@ -1,9 +1,43 @@
-import { fadeToDirection } from "./utils/AnimationSettings";
+import { gsap } from 'gsap';
+import { fadeToDirection, triggerAnimation } from "./utils/AnimationSettings";
 
 
 fadeToDirection(
-    '.home-item h1',
-    .5,
-    'y',
-    40
+    '.home-item h1', .5, 'y', 40
 );
+
+fadeToDirection(
+    '.home-item h2', 1, 'y', 30
+)
+
+fadeToDirection(
+    '.home-cta a:nth-child(1)', 1.5, 'x', -30
+)
+
+fadeToDirection(
+    '.home-cta a:nth-child(2)', 1.5, 'x', 30
+)
+
+fadeToDirection(
+    '.home-item h3', 1.5, 'y', -30
+)
+
+fadeToDirection(
+    '.home-item:nth-child(2)', 2, 'y', 30
+)
+
+triggerAnimation(
+    '.about-item', 'y', 40, 0, .8
+)
+
+triggerAnimation(
+    '.about-flex-item', 'x', -30, .5, .8
+)
+
+triggerAnimation(
+    '.try-feature', 'y', 30, 0, 1
+)
+
+triggerAnimation(
+    '.item-content', 'x', 30, .5, 1
+)
