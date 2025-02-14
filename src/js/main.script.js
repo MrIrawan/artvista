c3bde060d33b710f147389cbc17fe6d25fd15804
farrel irawan <irawanssfarrel@gmail.com>
2025-02-14 18:54:11 +0700

    merender elemen list sebanyak data topics yang di ambil dari API unsplash

---
+++ src/js/main.script.js
@@ -0,0 +1,22 @@
+import { getDataFromAPI } from "./utils/API.js";
+
+const listContainer = document.getElementById('collections-container');
+const apiUrl = `https://api.unsplash.com/topics?per_page=20&client_id=xHA1LFvj7tkMQdgmzgfJH7iCn2BXd5BwJYX4TxLaIv4`;
+
+async function getTopicsList() {
+    try {
+        const topicsData = await getDataFromAPI(apiUrl)
+        topicsData.forEach(topic => {
+            listContainer.insertAdjacentHTML('beforeend', `
+                <li><a href="#">${topic.title}</a></li>
+                
+            `);
+        });
+    } catch (err) {
+        console.error(`error message: ${err}`);
+    }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    getTopicsList();
+});
