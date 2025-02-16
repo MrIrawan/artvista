17f9404b7942412e1f5751229da9a70c546f57d4
farrel irawan <irawanssfarrel@gmail.com>
2025-02-16 08:48:43 +0700

    modifikasi pada html css dan javascript

--- src/js/main.script.js
+++ src/js/main.script.js
@@ -1,6 +1,6 @@
 import { getDataFromAPI } from "./utils/API.js";
 
-const listContainer = document.getElementById('collections-container');
+const listContainer = document.getElementById('topics-container');
 const apiUrl = `https://api.unsplash.com/topics?per_page=20&client_id=xHA1LFvj7tkMQdgmzgfJH7iCn2BXd5BwJYX4TxLaIv4`;
 
 async function getTopicsList() {
