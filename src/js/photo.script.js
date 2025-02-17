0858af7cbe91af648cb327b0333e98fc9002cc1f
farrel irawan <irawanssfarrel@gmail.com>
2025-02-17 19:51:38 +0700

    membuat komponen photo details menjadi dinamis

--- src/js/photo.script.js
+++ src/js/photo.script.js
@@ -8,6 +8,35 @@ const detailsContainer = document.getElementById('details-container');
 async function getPhotoDetails() {
     try {
         const dataResponse = await getDataFromAPI(apiUrl);
+        detailsContainer.innerHTML = `
+        <div class="header-details">
+            <img src="${dataResponse.urls.raw}" alt="${dataResponse.alt_description}">
+        </div>
+        <div class="body-details">
+            <div class="top-description">
+                <div class="top-description-item">
+                    <span>
+                        <p>Views</p>
+                        <p>${dataResponse.views}</p>
+                    </span>
+                    <span>
+                        <p>Download</p>
+                        <p>${dataResponse.downloads}</p>
+                    </span>
+                    <span>
+                        <p>Featured In</p>
+                        <p>hello, world, lorem ipsum</p>
+                    </span>
+                </div>
+                <div class="top-description-item">
+                    <a href="#">
+                        <span class="material-symbols-sharp">download</span>
+                        Download
+                    </a>
+                </div>
+            </div>
+        </div>
+        `
     } catch (err) {
         console.error(`error message : ${err}`);
     }
