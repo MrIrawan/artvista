32a9ad6dec1751316ce03a64fa611b8347704880
farrel irawan <irawanssfarrel@gmail.com>
2025-02-17 12:40:46 +0700

    menambahkan fungsi redirect halaman dan memberi parameter id pada url

--- src/js/main.api.js
+++ src/js/main.api.js
@@ -18,6 +18,7 @@ async function getPhotoFromAPI(apiUrl) {
         photoItems.forEach(item => {
             item.addEventListener('click', () => {
                 const photoId = item.getAttribute('data-id');
+                window.location.href = `photo.html?id=${photoId}`
             })
         })
         
