4f262656c2a976715d42ffc903194e837707354f
farrel irawan <irawanssfarrel@gmail.com>
2025-02-23 11:27:06 +0700

    membuat funngsi mengambil list detail collections

--- src/js/collections.script.js
+++ src/js/collections.script.js
@@ -1,4 +1,5 @@
 import { getDataFromAPI } from "./utils/API.js";
+import { CatchURIParams } from "./utils/CatchURIParams.js";
 
 const listContainer = document.getElementById('topics-container');
 const apiUrl = `https://api.unsplash.com/topics?per_page=16&client_id=xHA1LFvj7tkMQdgmzgfJH7iCn2BXd5BwJYX4TxLaIv4`;
@@ -19,6 +20,20 @@ async function getTopicsList() {
     }
 }
 
+async function getCollectionsDetails() {
+    const collectionsId = CatchURIParams();
+    const collectionsUrl = `https://api.unsplash.com/collections/:${collectionsId}?client_id=xHA1LFvj7tkMQdgmzgfJH7iCn2BXd5BwJYX4TxLaIv4`;
+
+    try {
+        const collectionsData = await getDataFromAPI(collectionsUrl);
+        console.log(collectionsData);
+        
+    } catch (err) {
+
+    }
+}
+
 document.addEventListener('DOMContentLoaded', () => {
     getTopicsList();
+    getCollectionsDetails();
 });
