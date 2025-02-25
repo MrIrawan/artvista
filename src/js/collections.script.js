6b3feee712354c9abe80f63ed299b1db868c96d0
farrel irawan <irawanssfarrel@gmail.com>
2025-02-25 16:09:28 +0700

    membuat komponen merender data secara dinamis

--- src/js/collections.script.js
+++ src/js/collections.script.js
@@ -1,10 +1,12 @@
 import { getDataFromAPI } from "./utils/API.js";
 import { CatchURIParams } from "./utils/CatchURIParams.js";
 import { CollectionsDetailsHandler } from "./utils/CollectionsDetailsHandler.js";
+import { DateFormatter } from "./utils/DateFormatter.js";
 
 const listContainer = document.getElementById('topics-container');
 const apiUrl = `https://api.unsplash.com/topics?per_page=16&client_id=xHA1LFvj7tkMQdgmzgfJH7iCn2BXd5BwJYX4TxLaIv4`;
 const artVistaTitle = document.getElementById('artvista-title');
+const collectionsContainer = document.getElementById('collections-container');
 
 async function getTopicsList() {
     try {
@@ -29,6 +31,29 @@ async function getCollectionsDetails() {
 
     try {
         const collectionsData = await getDataFromAPI(collectionsUrl);
+        collectionsContainer.innerHTML = `
+            <div class="collections-header">
+                <div class="header-description">
+                    <h2>${collectionsData.title}</h2>
+                    <p>${collectionsData.description}</p>
+                    <div class="other-description">
+                        <span id="description-wrapper">
+                            <span class="material-symbols-sharp">calendar_today</span>
+                            <p>published at ${DateFormatter(collectionsData.published_at)}</p>
+                        </span>
+                        <hr>
+                        <span id="description-wrapper">
+                            <span class="material-symbols-sharp">image</span>
+                            <p>up to ${collectionsData.total_photos} photo's</p>
+                        </span>
+                    </div>
+                </div>
+                <div class="header-image">
+                    <img src="${collectionsData.cover_photo.urls.regular}" alt="image ${collectionsData.id}">
+                </div>
+            </div>
+            <div class="collections-body"></div>
+        `;
         console.log(collectionsData);
         
     } catch (err) {
